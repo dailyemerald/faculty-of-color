7c2b4e4f80bd20c24aa850027d126664a1124cb2
Sierra Morgan <sierramorgan07@gmail.com>
2014-10-28 00:28:55 -0700

    committing theeee changes

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,10 @@ $(window).scroll(function(e) {
 
 var scrollAmount = $('body').scrollTop();
   console.log(scrollAmount);
-});
 
+
+if(scrollAmount >="1800") {
+	document.getElementById("vid").setAttribute('autoplay', true);
+};
+});
 });
