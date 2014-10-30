12d41e2f78fa33f1a4f500f892aabcd68eeba6fe
Sierra Morgan <sierramorgan07@gmail.com>
2014-10-30 00:26:04 -0700

    added back vid heading

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,10 @@ $(window).scroll(function () {
     var scrollAmount = $('body').scrollTop();
     console.log(scrollAmount);
 
-    if (scrollAmount >= "890" && scrollAmount <= "1490") {
+    if (scrollAmount >= "1708" && scrollAmount <= "2400") {
         document.getElementById("vid").setAttribute('autoplay', true);
+    }
+    if (scrollAmount >= "3290" && scrollAmount <= "4010") {
+    	document.getElementById("vid-2").setAttribute('autoplay', true);
     }
 });
