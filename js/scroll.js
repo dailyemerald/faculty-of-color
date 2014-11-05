9396ee2ec6d60ffcc3ca304e9308743437c9be36
Sierra Morgan <sierramorgan07@gmail.com>
2014-11-04 23:50:46 -0800

    added vid

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ var navShow = $(window).scroll(function () {
 
     var when = $(window).scrollTop();
 
-    if (when > 840) {
+    if (when > 870) {
         $(".banner").slideDown();
     } else if (when < 880) {
         $(".banner").slideUp();
@@ -11,41 +11,3 @@ var navShow = $(window).scroll(function () {
     console.log(when);
 });
 
-$(function () {
-    $(window).scroll(function () {
-
-        var scrollAmount = $('body').scrollTop();
-        console.log(scrollAmount);
-
-        if (scrollAmount == "1418" && scrollAmount<"1422") {
-
-            $("#videoHolder").html(
-                '<video width="100%" autoplay controls preload="auto">' +
-
-                '<source src="img/InnovationLamina.mp4" width="100%"></source>' +
-
-                '</video>'); 
-        } else if(scrollAmount > "1668" || scrollAmount < "1668") {
-        	$("videoHolder").html('autoplay', false); 
-        }
-    });
-});
-$(function () {
-    $(window).scroll(function () {
-
-        var scrollAmount = $('body').scrollTop();
-        console.log(scrollAmount);
-
-        if (scrollAmount == "3983" && scrollAmount<"3990") {
-
-            $("#videoCont").html(
-                '<video width="100%" autoplay controls>' +
-
-                '<source src="img/" width="100%"></source>' +
-
-                '</video>'); 
-        } else if(scrollAmount > "4088") {
-        	$("videoCont").html('autoplay', false);
-        }
-    });
-});
